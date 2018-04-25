834f392d3aeea08ee4354fc5fc1055c890cd5e21
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-04-25 12:57:47 +0200

    Initial commit

---
+++ src/shoreline_examproject/BLL/Config.java
@@ -0,0 +1,10 @@
+package shoreline_examproject.BLL;
+
+/**
+ * Maps relations between input and output data.
+ * @author sebok
+ */
+public class Config
+{
+    
+}
---
+++ src/shoreline_examproject/Utility/EventLog.java
@@ -0,0 +1,10 @@
+package shorelineexam.Utility;
+
+/**
+ * Represent a logged event (for example, an error).
+ * @author sebok
+ */
+public class EventLog
+{
+    
+}
