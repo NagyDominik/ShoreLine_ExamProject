d8d133b7feae7b97f8ddec2f16b3934353b0685c
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-05-20 12:49:58 +0200

    DBConfig add/load Functions implemented

--- src/shoreline_examproject/BLL/BLLManager.java
+++ src/shoreline_examproject/BLL/BLLManager.java
@@ -1,6 +1,7 @@
 package shoreline_examproject.BLL;
 
 import java.io.IOException;
+import java.util.List;
 import javafx.beans.property.BooleanProperty;
 import shoreline_examproject.BE.AttributesCollection;
 import shoreline_examproject.BE.Config;
@@ -13,13 +14,13 @@ import shoreline_examproject.DAL.IDataAccess;
 
 public class BLLManager implements IBLLManager {
 
-    private IDataAccess dal;
+    private IDataAccess dalManager;
     private Converter converter;
     private FolderHandler folderHandler;
     
     public BLLManager() throws BLLException {
         try {
-            this.dal = new DALManager();
+            this.dalManager = new DALManager();
             this.converter = new Converter(this);
             this.folderHandler = new FolderHandler(this);
         } catch (IOException ex) {
@@ -29,22 +30,27 @@ public class BLLManager implements IBLLManager {
 
     @Override
     public void saveLog(EventLog log) {
-        dal.saveLog(log);
+        dalManager.saveLog(log);
     }
 
     @Override
     public void saveConfig(Config config) {
-        dal.saveConfig(config);
+        dalManager.saveConfig(config);
+    }
+    
+    @Override
+    public List<Config> loadConfigs() {
+        return dalManager.loadConfigs();
     }
 
     @Override
     public AttributesCollection loadFileData(String filePath) {
-        return dal.loadFileData(filePath);
+        return dalManager.loadFileData(filePath);
     }
 
     @Override
     public void saveToJSON(AttributesCollection data) {
-        dal.saveData(data);
+        dalManager.saveData(data);
     }
 
     @Override
@@ -99,4 +105,5 @@ public class BLLManager implements IBLLManager {
             throw new BLLException(ex);
         }
     }
+
 }
